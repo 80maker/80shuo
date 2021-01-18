932ad127b1e4a59b996755b6da73393c05bfc3ae
cmgddd <cmgddd@163.com>
2021-01-18 22:57:44 +0800

    init

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,94 @@
+module.exports = {
+  title: '1980\'s Maker',
+  description: '一个出生于80年代的程序员 -- 喜爱创[客]、美[食]、动[画]、怀[旧]的新手艺人',
+  port: 8080,
+  theme: 'vuepress-theme-maker',
+  themeConfig: {
+    logo: '/logo.jpg',
+    subNav: [],
+    nav: [
+      { text: '🏠 Home', link: '/' },
+      { text: '📖 Theme', link: '/categories/theme/' },
+      { text: '🐉 Maker', link: '/categories/maker/' },
+      { text: '🔥 Animation', link: '/categories/animation/' },
+      { text: '📽 Old Time', link: '/categories/oldtime/' },
+      { text: '🔗 friend-links', link: '/friend-links/' },
+    ],
+    searchPlaceholder: 'Search',
+    searchMaxSuggestions: 10,
+    social: [
+      {
+        type: 'email',
+        link: 'cmgddd@163.com'
+      },
+      {
+        type: 'github',
+        link: '80maker/vuepress-theme-maker'
+      },
+      {
+        type: 'qq',
+        link: '//qm.qq.com/cgi-bin/qm/qr?k=fknyQ434nkzVUWUmJ6rpIPctkS9eyQaZ&jump_from=webapi'
+      },
+      {
+        type: 'feed',
+        link: '/rss.xml'
+      }
+    ],
+    copyright: '© 2020 ❤️ <a target="_blank" href="https://17ria.com/">Neil Chen</a>'
+  },
+  plugins: [
+    ['@vuepress/search', {
+      searchMaxSuggestions: 10
+    }],
+    ['@vuepress/blog', {
+      globalPagination: {
+        prevText: '',
+        nextText: ''
+      },
+      directories: [
+        {
+          id: 'post',
+          dirname: '_post',
+          path: '/post/',
+          itemPermalink: '/post/:year/:month/:day/:slug.html',
+          pagination: {
+            perPagePosts: 10,
+          },
+        }
+      ],
+      frontmatters: [
+        {
+          id: "tag",
+          keys: ['tag', 'tags'],
+          path: '/tags/',
+          frontmatter: { title: 'Tag' },
+          pagination: {
+            lengthPerPage: 10
+          }
+        },
+        {
+          id: "category",
+          keys: ['category', 'categories'],
+          path: '/categories/',
+          frontmatter: { title: 'Category' },
+          pagination: {
+            lengthPerPage: 10
+          }
+        }
+      ],
+      sitemap: {
+        hostname: 'https://80shuo.com'
+      },
+      feed: {
+        canonical_base: 'http://80shuo.com',
+      },
+      comment: {
+        service: 'vssue',
+        owner: '80maker',
+        repo: '80maker.github.io',
+        clientId: 'Iv1.57b5c522319529f6',
+        clientSecret: '7ad0e3d1455a2d6425c813b37a0526e9bd820657',
+      }
+    }]
+  ]
+}
---
+++ blog/_post/md5.md
@@ -0,0 +1,43 @@
+---
+date: 2020-11-26
+category: frontend
+title: 浏览器端不用第三方包也可以获取md5值
+---
+
+> 利益于JS的同构特性,crypto虽然是Node.js的自带模块,但同时也能用于Web浏览器端。
+
+<!-- more -->
+## 双主角
+
+### crypto
+
+::: warning
+这家伙是nodejs自带模块,测试了下在浏览器端也能正常使用
+:::
+
+### FileReader
+
+## 隐藏的孪生兄弟
+
+### atob和btoa
+
+## 关键代码
+``` js {5}
+// 将reader方法封装成Promisify 
+async readFileData(fileReader, file) {
+  return new Promise((resolve, reject) => {
+    fileReader.addEventListener('load', (ev) => {
+      resolve(ev.target.result);
+    });
+    fileReader.readAsArrayBuffer(file);
+  });
+}
+
+// 代码调用
+const reader = new FileReader();
+const crypto = require('crypto');
+const md5 = crypto.createHash('md5');
+let fileBuff = await that.readFileData(reader, file);
+fileBuff = Buffer.from(fileBuff);
+md5Str = md5.update(fileBuff).digest('hex');
+```
