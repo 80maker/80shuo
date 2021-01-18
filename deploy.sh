bea61a245037fe26aec041fe93fdc159a3122b84
cmgddd <cmgddd@163.com>
2021-01-18 23:07:03 +0800

    up

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:80maker/80maker.github.io.git master:gh-pages
+git push -f git@github.com:80maker/80shuo.git master:gh-pages
 
 cd -
