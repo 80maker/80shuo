c3e0293adbb19000642f0d70984cc28f7dc616b3
cmgddd <cmgddd@163.com>
2021-01-20 22:47:21 +0800

    up

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -5,7 +5,13 @@ module.exports = {
   theme: 'vuepress-theme-maker',
   markdown: {
     lineNumbers: true,
-    extractHeaders: [ 'h2', 'h3', 'h4' ]
+    extractHeaders: [ 'h2', 'h3', 'h4' ],
+    plugins: {
+      'markdown-it-mark': true,
+      'markdown-it-footnote': true,
+      'markdown-it-abbr': true,
+      'markdown-it-task-lists': true
+    }
   },
   themeConfig: {
     logo: '/logo.jpg',
